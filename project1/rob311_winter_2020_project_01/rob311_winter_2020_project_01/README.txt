9cb79f5c25b6e96f3fb5e7fb54fd9fd550551617
terrymei <zhixinterry@gmail.com>
2020-02-05 00:39:36 -0500

    project 1: implemented BFS and BiDirSearch

---
+++ project1/rob311_winter_2020_project_01/rob311_winter_2020_project_01/README.txt
@@ -0,0 +1,15 @@
+# General 
+
+Do not import any libraries into the code.  All neccessary libraries have already been included in the template files. Do not modify the form of the import statements either (e.g., by importing individual functions from numpy: just access them through np.function_name()). 
+
+# Testing your code
+
+The template files include sample tests. Be sure to explore the data and make your own!
+
+# Submitting your code 
+
+When you are ready to submit, archive the files with the following command:
+
+tar cvf handin.tar breadth_first_search.py bidirectional_search.py a_star_search.py
+
+Then upload handin.tar to http://courses.jonathankelly.info/courses/ROB311H1/assessments/searchalgorithms/
---
+++ project1/rob311_winter_2020_project_01/rob311_winter_2020_project_01/a_star_search.py
@@ -0,0 +1,57 @@
+import queue
+import numpy as np
+from search_problems import Node, GridSearchProblem, get_random_grid_problem
+
+
+def a_star_search(problem):
+    """
+    Uses the A* algorithm to solve an instance of GridSearchProblem. Use the methods of GridSearchProblem along with
+    structures and functions from the allowed imports (see above) to implement A*.
+
+    :param problem: an instance of GridSearchProblem to solve
+    :return: path: a list of states (ints) describing the path from problem.init_state to problem.goal_state[0]
+             num_nodes_expanded: number of nodes expanded by your search
+             max_frontier_size: maximum frontier size during search
+    """
+    ####
+    #   COMPLETE THIS CODE
+    ####
+    num_nodes_expanded = 0
+    max_frontier_size = 0
+    path = []
+    return path, num_nodes_expanded, max_frontier_size
+
+
+def search_phase_transition():
+    """
+    Simply fill in the prob. of occupancy values for the 'phase transition' and peak nodes expanded within 0.05. You do
+    NOT need to submit your code that determines the values here: that should be computed on your own machine. Simply
+    fill in the values!
+
+    :return: tuple containing (transition_start_probability, transition_end_probability, peak_probability)
+    """
+    ####
+    #   REPLACE THESE VALUES
+    ####
+    transition_start_probability = -1.0
+    transition_end_probability = -1.0
+    peak_nodes_expanded_probability = -1.0
+    return transition_start_probability, transition_end_probability, peak_nodes_expanded_probability
+
+
+if __name__ == '__main__':
+    # Test your code here!
+    # Create a random instance of GridSearchProblem
+    p_occ = 0.25
+    M = 10
+    N = 10
+    problem = get_random_grid_problem(p_occ, M, N)
+    # Solve it
+    path, num_nodes_expanded, max_frontier_size = a_star_search(problem)
+    # Check the result
+    correct = problem.check_solution(path)
+    print("Solution is correct: {:}".format(correct))
+    # Plot the result
+    problem.plot_solution(path)
+
+    # Experiment and compare with BFS
