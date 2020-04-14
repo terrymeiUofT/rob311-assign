ec54decc67f5a91b837fcc7132d49acd9ac1b48d
terrymei <zhixinterry@gmail.com>
2020-04-14 01:06:13 -0400

    finished

--- final_project/python/rl_agent.py
+++ final_project/python/rl_agent.py
@@ -6,35 +6,114 @@ import numpy as np
 """
 Agent Description:
 
-TODO: Insert a 10-15 line description (80 characters wide) of the algorithm
-that you implemented in your agent. If you used a reference paper or book,
-you may add additional lines to cite this reference.
+The Reinforcement Learning strategy being used to train this CartPoleAgent is 
+the REINFORCE policy gradient method. The major distinction of this method 
+from other RL strategies is its weight update frequency: REINFORCE method 
+updates the weight after the terminal state is reached in an episode.
+
+Some of the key techniques utilized in this method is listed below:
+- The probability distribution for each action (L or R) is calculated using 
+  exponential soft-max function. 
+- At each step, an action is chosen randomly with the guidance of a 
+  probability distribution. This distribution is the same within each episode.
+- The weight is a function of the gradient at each step and the  discounted 
+  future reward[1](equation 13.8). The techniques for computing gradients of 
+  the policy is found from this source[2]. The reward function is biased 
+  towards the action taken in near future.
+
+References:
+[1]R. Sutton and A. Barto, Reinforcement learning.
+[2]S. Kirkiles, "REINFORCE Policy Gradients From Scratch In Numpy", Medium, 2018
+   [Online]. Available: https://medium.com/samkirkiles/reinforce-policy
+   -gradients-from-scratch-in-numpy-6a09ae0dfe12. [Accessed: 13-Apr-2020].
 """
+
 
 class CartPoleAgent:
 
     def __init__(self, observation_space, action_space):
-        #----- TODO: Add your code here. -----
-
+        # ----- TODO: Add your code here. -----
         # Store observation space and action space.
         self.observation_space = observation_space
         self.action_space = action_space
+        # added fields
+        self.weight = np.random.rand(4, 2)
+        self.action_prob = None
+        self.grad_history = []
+        self.reward_history = []
+        self.gamma = 0.99
+        self.lr = 0.001
+        self.change_lr = False
+        self.last_20_epi_reward = []
 
     def action(self, state):
         """Choose an action from set of possible actions."""
-        #----- TODO: Add your code here. -----
+        # ----- TODO: Add your code here. -----
+        # reshape state to (4, 2) to match the shape of the weight
+        state = state[None, :]
 
-        # Dummy agent just takes random actions...
-        action = random.randint(0, self.action_space.n - 1)
-        #print("Chose action " + str(action))
+        # compute the prob. of taking each action using softmax
+        dot_product = state.dot(self.weight)[0]
+        softmax_numer = np.exp(dot_product)
+        softmax_denom = np.sum(softmax_numer)
+        self.action_prob = softmax_numer / softmax_denom
+
+        # choose the action randomly given the probability distribution
+        action = np.random.choice(self.action_space.n, p=self.action_prob)
+        # print("Chose action " + str(action))
         return action
 
     def reset(self):
         """Reset the agent, if desired."""
-        #----- TODO: Add your code here. -----
-        pass
+        # ----- TODO: Add your code here. -----
+        self.grad_history = []
+        self.reward_history = []
 
     def update(self, state, action, reward, state_next, terminal):
         """Update the agent internally after an action is taken."""
-        #----- TODO: Add your code here. -----
-        pass
+        # ----- TODO: Add your code here. -----
+        # compute the Jacobian matrix for softmax
+        reshaped_prob = self.action_prob.reshape(2, 1)
+        Jacob_mat = np.diagflat(reshaped_prob) - \
+                    reshaped_prob.dot(np.transpose(reshaped_prob))
+
+        # find the gradient of log policy
+        d_policy = Jacob_mat[action, :]
+        policy = self.action_prob[action]
+        d_log_policy = d_policy / policy
+        gradient = (np.transpose(state).reshape(4, 1)).dot(
+            d_log_policy.reshape(1, 2))
+
+        # update the history of gradient and reward
+        self.grad_history.append(gradient)
+        self.reward_history.append(reward)
+
+        # update the weight if reach terminal
+        if terminal:
+            for t in range(len(self.grad_history)):
+                accum_reward = 0
+                for i in range(t, len(self.reward_history)):
+                    accum_reward += (self.gamma ** (i-t)) * \
+                                    (self.reward_history[t])
+                self.weight += self.lr * self.grad_history[t] * accum_reward
+
+            # lower the learning rate if reward is constantly high
+            episode_reward = sum(self.reward_history)
+            if len(self.last_20_epi_reward) < 20:
+                self.last_20_epi_reward.append(episode_reward)
+            else:
+                self.last_20_epi_reward.pop(0)
+                self.last_20_epi_reward.append(episode_reward)
+
+            if self.change_lr:
+                pass
+            else:
+                if len(self.last_20_epi_reward) == 20:
+                    high_count = 0
+                    for i in range(20):
+                        if self.last_20_epi_reward[i] > 300:
+                            high_count += 1
+                    if high_count > 15:
+                        self.change_lr = True
+                    if self.change_lr:
+                        self.lr = 0.0001
--- final_project/python/test_cart_pole.py
+++ final_project/python/test_cart_pole.py
@@ -3,9 +3,13 @@ import numpy as np
 
 from rl_agent import CartPoleAgent
 
+
 # Test harness used to evaluate a CartPoleAgent.
 SUCCESS_REWARD = 195
 SUCCESS_STREAK = 100
+MAX_EPISODES = 1500
+MAX_STEPS = 5000
+
 
 def run_cart_pole():
     """
@@ -16,54 +20,58 @@ def run_cart_pole():
     of 195 or more over 100 consective episodes.
     """
     env = gym.make("CartPole-v0")
+    env._max_episode_steps = MAX_STEPS
 
     # Create an instance of the agent.
     cp_agent = CartPoleAgent(env.observation_space, env.action_space)
-    total_reward, avg_reward, last_reward, win_streak = (0, 0, 0, 0)
+    avg_reward, win_streak = (0, 0)
+    rewards = []
 
-    for episode in range(1500):
+    for episode in range(MAX_EPISODES):
         state = env.reset()
 
         # Reset the agent, if desired.
         cp_agent.reset()
-        this_reward = 0
+        episode_reward = 0
     
         # The total number of steps is limited (to avoid long-running loops).
-        for steps in range(5000):
+        for steps in range(MAX_STEPS):
             #env.render()
 
             # Ask the agent for the next action and step accordingly.
             action = cp_agent.action(state)
             state_next, reward, terminal, info = env.step(action)
             reward = reward if not terminal else -reward
-            this_reward += reward # Total reward for this episode.
 
             # Update any information inside the agent, if desired.
             cp_agent.update(state, action, reward, state_next, terminal)
+            episode_reward += reward # Total reward for this episode.
+            state = state_next
 
             if terminal:
                 # Update average reward.
                 if episode < SUCCESS_STREAK:
-                    total_reward += this_reward
-                    avg_reward = float(total_reward)/(episode + 1)
+                    rewards.append(episode_reward)
+                    avg_reward = float(sum(rewards))/(episode + 1)
                 else:
                     # Last set of epsiodes only (moving average)...
-                    total_reward = total_reward + this_reward - last_reward
-                    avg_reward = float(total_reward)/SUCCESS_STREAK 
+                    rewards.append(episode_reward)
+                    rewards.pop(0)
+                    avg_reward = float(sum(rewards))/SUCCESS_STREAK
 
                 # Print some stats.
                 print("Episode: " + str(episode) + \
-                      ", Reward: " + str(this_reward) + \
+                      ", Reward: " + str(episode_reward) + \
                       ", Avg. Reward: " + str(avg_reward))
   
-                last_reward = this_reward
-
                 # Is the agent on a winning streak?
-                if reward >= SUCCESS_REWARD:
+                if episode_reward >= SUCCESS_REWARD:
                     win_streak += 1
                 else:
                     win_streak = 0
                 break
+
+        #print(rewards)
 
         # Has the agent succeeded?
         if win_streak == SUCCESS_STREAK and avg_reward >= SUCCESS_REWARD:
@@ -72,8 +80,9 @@ def run_cart_pole():
     # Worst case, agent did not meet criterion, so bail out.
     return episode + 1, avg_reward
 
+
 if __name__ == "__main__":
-    episodes, best_avg_reward = run_cart_pole()
+    episodes, achieved_avg_reward = run_cart_pole()
     print("--------------------------")
     print("Episodes to solve: " + str(episodes) + \
-          ", Best Avg. Reward: " + str(best_avg_reward))
+          ", Achieved Avg. Reward: " + str(achieved_avg_reward))
